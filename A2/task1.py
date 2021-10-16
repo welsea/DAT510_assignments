8fc0ed8510700684f36a816a6e251facb43b7b99
welsea <welsea.42@gmail.com>
2021-10-16 21:44:43 +0200

    format

--- A2/task1.py
+++ A2/task1.py
@@ -3,15 +3,13 @@ import CSPRNG
 
 
 # Diffie-hellman key exchange
-seed=DH.dh()
+seed = DH.dh()
 
 # CSPRNG-RC4
-k=CSPRNG.rc4(seed)
-print("The key that will be used for encryption and decryption:",k)
+k = CSPRNG.rc4(seed)
+print("The key that will be used for encryption and decryption:", k)
 
-#encrypt
-
-#decrypt
+# encrypt
 
 
-
+# decrypt
